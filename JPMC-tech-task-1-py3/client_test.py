c229ffdfec4584904c30e5b066ef01deac8f6d84
sreeharisangeeth <sreeharisangeeth@gmail.com>
2020-11-26 07:32:22 +0530

    Initial commit

---
+++ JPMC-tech-task-1-py3/client_test.py
@@ -0,0 +1,41 @@
+import unittest
+from client3 import getDataPoint,getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Assertion Added ------------ """
+    for quote in quotes:
+          self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Assertion Added ------------ """
+    for quote in quotes:
+          self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+
+
+  """ ------------ Unit tests Added ------------ """
+
+  def test_getRatio_findRatio(self):
+    prices=[[119.2,120.1],[122.4,115.7]]
+    for price_A,price_B in prices:
+          self.assertEqual(getRatio(price_A,price_B),price_A/price_B)
+
+  def test_getRatio_findRatiodivisionByZero(self):
+        self.assertEqual(getRatio(120,0),None)
+          
+          
+        
+        
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ README.md
@@ -0,0 +1,2 @@
+# JP_Morgan_Virtual_Internship
+ 
